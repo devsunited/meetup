af5d6941a154b3c2b313c55c7489c274be025952
Martin Hochel <hochelmartin@gmail.com>
2016-05-15 18:11:44 +0200

    feat(app): add layout and MD scaffold

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
+import { HTTP_PROVIDERS } from '@angular/http';
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+
 import { HuffelpuffCounterContainerComponent } from './containers/huffelpuff-counter-container/huffelpuff-counter-container.component';
 
 @Component({
   moduleId: module.id,
   selector: 'du-app',
-  directives: [HuffelpuffCounterContainerComponent],
+  directives: [
+    MD_SIDENAV_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdIcon,
+    MdToolbar,
+    HuffelpuffCounterContainerComponent
+  ],
+  providers:[
+    HTTP_PROVIDERS,
+    MdIconRegistry
+  ],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {}
+export class AppComponent { }
